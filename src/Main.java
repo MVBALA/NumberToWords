e590a2e16e863a681c7db05e2ebfa585c747c52e
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-25 09:54:45 +0530

    it is udemy exercise.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       NumberToWords.numberToWords(10);
+       NumberToWords.numberToWords(4409);
+
     }
 }
--- src/NumberToWords.java
+++ src/NumberToWords.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class NumberToWords {
+public class NumberToWords {
+    public static void numberToWords(int number) {
+        if (number < 0)
+            System.out.println("Invalid Value");
+        else {
+            int c = getDigitCount(number);
+            number = reverse(number);
+            while (c!= 0) {
+
+                switch (number%10) {
+                    case 1:
+                        System.out.println("One");
+                        break;
+                    case 2:
+                        System.out.println("Two");
+                        break;
+                    case 3:
+                        System.out.println("Three");
+                        break;
+                    case 4:
+                        System.out.println("Four");
+                        break;
+                    case 5:
+                        System.out.println("Five");
+                        break;
+                    case 6:
+                        System.out.println("Six");
+                        break;
+                    case 7:
+                        System.out.println("Seven");
+                        break;
+                    case 8:
+                        System.out.println("Eight");
+                        break;
+                    case 9:
+                        System.out.println("Nine");
+                        break;
+                    case 0:
+                        System.out.println("Zero");
+                        break;
+                }
+                number /= 10;
+                c--;
+            }
+        }
+    }
+
+    public static int reverse(int number) {
+        int n = number;
+        int sum = 0;
+        while (n != 0) {
+            int x = n % 10;
+            sum = (sum * 10) + x;
+            n /= 10;
+        }
+        return sum;
+    }
+
+    public static int getDigitCount(int number) {
+        int c = 1;
+        if (number < 0) {
+            return -1;
+        } else {
+            while (number >9) {
+                number /= 10;
+                ++c;
+            }
+            return c;
+        }
+    }
 }
